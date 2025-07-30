910b121e1aa611ef966d8168357671db25c9576b
jony <jonymoin39@gmail.com>
2025-07-30 11:05:57 +0600

    🛠️ Fix build config and remove content-encoding

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+  base: '/', // আগের './' এর পরিবর্তে absolute path ব্যবহার করছি
 
   plugins: [
     react(),

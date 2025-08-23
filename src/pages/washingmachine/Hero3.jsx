321565ae8975741c08003e16437bfaeb825e63e9
jony <jonymoin39@gmail.com>
2025-08-23 08:35:34 +0600

    access issue solve1

--- src/pages/washingmachine/Hero3.jsx
+++ src/pages/washingmachine/Hero3.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import {
-  LuWashingMachine
-} from 'react-icons/lu';
-import {
-  BiDish, BiCctv
-} from 'react-icons/bi';
-
-
-import {
-  TbAirConditioning
-} from 'react-icons/tb';
-
-
-import {
-  Link
-} from 'react-router-dom';
+import { LuWashingMachine } from 'react-icons/lu';
+import { BiDish } from 'react-icons/bi';
+import { TbAirConditioning } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
 
 const Hero3 = () => {
   const services = [
     { id: 1, name: "Washing Machine", slug: "washing-machine", icon: LuWashingMachine },
     { id: 2, name: "Dishwasher Repair", slug: "dishwasher-repair", icon: BiDish },
     { id: 3, name: "Air-Conditioning Service", slug: "air-conditioning-service", icon: TbAirConditioning },
-   
-    
   ];
 
   return (
@@ -34,16 +20,18 @@ const Hero3 = () => {
             key={service.id}
             className="rounded-xl overflow-hidden shadow-lg bg-white"
           >
-            <div
-              className="h-[180px] bg-cover bg-center relative"
-              style={{ backgroundImage: `url(/images/service${index + 1}.webp)` }}
-            >
-              <Link to={`/service/${service.slug}`}>
-              <div className="absolute top-2 left-2 bg-white p-2 rounded-full text-[#00FFFF] text-2xl shadow">
-                <service.icon />
+            {/* Whole image area clickable */}
+            <Link to={`/service/${service.slug}`}>
+              <div
+                className="h-[180px] bg-cover bg-center relative"
+                style={{ backgroundImage: `url(/images/service${index + 1}.webp)` }}
+              >
+                <div className="absolute top-2 left-2 bg-white p-2 rounded-full text-[#00FFFF] text-2xl shadow">
+                  <service.icon />
+                </div>
               </div>
-              </Link>
-            </div>
+            </Link>
+
             <div className="p-4 text-center">
               <h3 className="text-xl font-bold mb-2">{service.name}</h3>
               <Link
